2bf38c0464c386ebfa63e630ef22dd4fe586895b
ReactorOriole <766196+ReactorOriole@users.noreply.github.com>
2023-03-05 00:08:32 -0800

    Adding neovim task and related setup files

---
+++ roles/common/files/nvim/init.lua
@@ -0,0 +1,22 @@
+
+require("plugins")
+
+vim.g.mapleader = "\\"
+
+vim.opt.mouse = ""
+
+-- Make Y work like other capitals, copy to end of line
+vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
+
+-- Save with sudo
+vim.cmd(":command! W w !sudo tee % > /dev/null")
+
+-- Make esc fast
+vim.o.ttimeoutlen = 0
+
+-- Source all lua directory files
+local paths = vim.split(vim.fn.glob("~/.config/nvim/lua/*lua"), "\n")
+
+for i, file in pairs(paths) do
+  vim.cmd("source " .. file)
+end
